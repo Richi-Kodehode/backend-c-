fd3713d124a140e4dd62971f84e6e270e5bfd3c8
Richi-Kodehode <richard25sfj@gmail.com>
2025-01-28 11:03:00 +0100

    setup main function

--- Program.cs
+++ Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿using System.Security.Cryptography;
+
+Console.WriteLine("Starting Renting Service");
+
+while (true)
+{
+    string? input = Console.ReadLine();
+    if (input == null)
+    {
+        Environment.Exit(1);
+    }
+
+    switch (input)
+    {
+        case "list":
+        Console.WriteLine("Listing books");
+        break;
+        case "borrow":
+        Console.WriteLine("Borrowing book");
+        break;
+        case "return":
+        Console.WriteLine("Returning book");
+        break;
+        default:
+        Console.WriteLine("Invalid Input");
+        break;
+    }
+}
