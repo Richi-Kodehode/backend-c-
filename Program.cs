90612f5facc0b963827406d0116f5b97325e9347
Richi-Kodehode <richard25sfj@gmail.com>
2025-01-29 09:22:22 +0100

    changed logic

--- Program.cs
+++ Program.cs
@@ -1,31 +1,34 @@
-﻿using System.Security.Cryptography;
-
-Console.WriteLine("Starting Renting Service");
+﻿
+﻿Console.WriteLine("Starting Renting Service");
 
 RentingService rentingService = new RentingService();
 
-
 while (true)
 {
-    string? input = Console.ReadLine();
-    if (input == null)
-    {
-        Environment.Exit(1);
-    }
+  string? input = Console.ReadLine();
+  if (input == null)
+  {
+    Environment.Exit(1);
+  }
 
-    switch (input)
-    {
-        case "list":
-        rentingService.ListAllBooks();
-        break;
-        case "borrow":
-        Console.WriteLine("Borrowing book");
-        break;
-        case "return":
-        Console.WriteLine("Returning book");
-        break;
-        default:
-        Console.WriteLine("Invalid Input");
-        break;
-    }
+  switch (input)
+  {
+    case "list":
+      Dictionary<Book, int> availableBooks = rentingService.ListAllBooks();
+      foreach (var bookEntry in availableBooks)
+      {
+        Console.WriteLine(bookEntry.Key.Title);
+      }
+      break;
+    case "borrow":
+      Console.WriteLine("Borrowing book");
+      break;
+    case "return":
+      Console.WriteLine("Returning book");
+      break;
+    default:
+      Console.WriteLine("Invalid Input");
+      break;
+  }
 }
+
--- RentingService.cs
+++ RentingService.cs
@@ -1,30 +1,43 @@
-class Book
-{
-    public string Title;
-    public string Author;
+using System.Net.Http.Headers;
 
-    public Book(string title, string author)
-    {
-        Title = title;
-        Author = author;
-
-    }
-
-}
 class RentingService
 {
-    private Dictionary<Book, int> bookInventory;
-    private Dictionary<Book, int> currentlyBorrowed;
+  private Dictionary<Book, int> bookInventory;
+  private Dictionary<Book, int> currentlyBorrowed;
 
-    public RentingService()
-    {
-        bookInventory = new Dictionary<Book, int>();
-        currentlyBorrowed = new Dictionary<Book, int>();
-    }
+  public RentingService()
+  {
+    //Lage sett med bøker
+    Book martian = new Book("Martian", "Jim");
+    Book foundation = new Book("Foundation", "Jack");
 
-    public void ListAllBooks(){
-        Console.WriteLine("Listing out books");
-    }
+    //Opprette datastruktur
+    bookInventory = new Dictionary<Book, int>();
+    currentlyBorrowed = new Dictionary<Book, int>();
+
+    //Legg til bøker
+    bookInventory.Add(martian, 10);
+    currentlyBorrowed.Add(martian, 0);
+    bookInventory.Add(foundation, 2);
+    currentlyBorrowed.Add(foundation, 0);
+  }
+
+  public Dictionary<Book, int> ListAllBooks()
+  {
+    return bookInventory;
+  }
+}
+
+class Book
+{
+  public string Title;
+  public string Author;
+
+  public Book(string title, string author)
+  {
+    Title = title;
+    Author = author;
+  }
 }
 
 class BorrowReciept
